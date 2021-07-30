89e96872dc8a54d0ccd851368ea4f76e98363cfb
Julie <julia.mysyuk@gmail.com>
2021-07-30 18:49:31 -0400

    Old phone key combinations.

---
+++ s14/b_combinatios.py
@@ -0,0 +1,28 @@
+key = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+}
+
+def recursion(dictionary, digits, idx, res, result):
+    if idx >= len(digits):
+        result.append(res)
+        return
+
+    digit = digits[idx]
+    letters = dictionary[digit]
+
+    for c in letters:
+        recursion(dictionary, digits, idx + 1, res + c, result)
+
+
+if __name__ == "__main__":
+    input = input()
+    result = []
+    recursion(key, input, 0, '', result)
+    print(*result)
